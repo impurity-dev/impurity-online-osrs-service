a4657fced055d604d97e2747c5daa89f655d6f5b
Tyler Kokoszka <techkoko77@gmail.com>
2019-07-03 20:53:31 -0400

    Initial Commit

---
+++ src/main/java/com/impurityonline/osrs/OsrsApplication.java
@@ -0,0 +1,13 @@
+package com.impurityonline.osrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsrsApplication.class, args);
+	}
+
+}
